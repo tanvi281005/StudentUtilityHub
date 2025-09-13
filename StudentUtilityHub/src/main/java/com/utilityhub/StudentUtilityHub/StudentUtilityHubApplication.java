643ea94765a41fc34de20c0710951261c8b23254
Tanvi <tanvi.bansal.cse23@itbhu.ac.in>
2025-09-13 15:16:02 +0530

    Initial commit - added SQL project

---
+++ StudentUtilityHub/src/main/java/com/utilityhub/StudentUtilityHub/StudentUtilityHubApplication.java
@@ -0,0 +1,13 @@
+package com.utilityhub.StudentUtilityHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentUtilityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentUtilityHubApplication.class, args);
+	}
+
+}
